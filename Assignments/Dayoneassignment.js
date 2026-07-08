5b7175511a3d164d5df67155822c909ba24b73c9
Viji163 <Viji1631995@gmail.com>
2026-07-08 15:38:22 +0530

    Add files via upload
    Completed first home assignment

---
+++ Assignments/Dayoneassignment.js
@@ -0,0 +1,25 @@
+
+let userName = "Thara"
+ console.log(userName);
+ console.log(typeof userName )
+
+ let organization ="23andMe"
+ console.log(organization);
+ console.log(typeof organization)
+
+ let contactNumber = +918798098765
+ console.log(contactNumber);
+ console.log(typeof contactNumber)
+
+ let knowsAutomation1 ="Yes"
+ console.log(knowsAutomation1);
+ console.log(typeof knowsAutomation1 );
+ 
+ let knowsAutomation = true
+ console.log(knowsAutomation);
+ console.log(typeof knowsAutomation);
+
+ let usesPlaywright
+ console.log(usesPlaywright);
+ console.log(typeof usesPlaywright);
+ 
