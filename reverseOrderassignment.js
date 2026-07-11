8e1bab4c1b329945da01db05b2ca07e335a7c410
Viji163 <Viji1631995@gmail.com>
2026-07-11 18:46:15 +0530

    Add files via upload
    Completed the home assignment for looping condition in reverse order

---
+++ reverseOrderassignment.js
@@ -0,0 +1,9 @@
+//use the for loop to iterate in reverse order and perform arithmetic operations in JavaScript
+function determineSqares() {
+    for (let i = 10; i >1; i--) {
+   console.log(i*i);
+   console.log("The sqaure of each number is"); 
+    
+}  
+}
+determineSqares()
